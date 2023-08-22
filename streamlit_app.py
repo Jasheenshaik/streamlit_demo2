11c9be438efb07464572407e364324a18e725088
Jasheenshaik <77780331+Jasheenshaik@users.noreply.github.com>
2023-08-22 16:45:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
-from transformers import AutoTokenizer 
+# from transformers import AutoTokenizer 
 # except:
 #     import os
 #     os.system("pip3 install transformers")
@@ -11,8 +11,8 @@ st.title("This is a demo website for the llm checking")
 
 
 def main():
-    model_name = 'bert-base-uncased'
-    tokenizer = AutoTokenizer.from_pretrained(model_name) 
+    # model_name = 'bert-base-uncased'
+    # tokenizer = AutoTokenizer.from_pretrained(model_name) 
     uploaded_file = st.file_uploader(label = 'upload the file')
     if uploaded_file is not None:
         try:
