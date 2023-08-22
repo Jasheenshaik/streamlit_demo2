197788d13737d89bf094a8a6d52658c363e45742
Jasheenshaik <77780331+Jasheenshaik@users.noreply.github.com>
2023-08-22 12:44:20 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd 
+# from transformers import AutoTokenizer 
+
+st.title("This is a demo website for the llm checking") 
+
+
+
+
+uploaded_file = st.file_uploader("Choose a file")
+
+dataframe = pd.read_csv(uploaded_file)
+    
+st.write(dataframe)
+
+# name =  st.text_input('enter your name')
+
+# st.write(f'the name you have entered is {name}')
