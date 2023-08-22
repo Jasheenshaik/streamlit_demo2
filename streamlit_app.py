15d04502508b62f42e87963b89038344d0ba7fbb
Jasheenshaik <77780331+Jasheenshaik@users.noreply.github.com>
2023-08-22 12:54:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ uploaded_file = st.file_uploader("Choose a file")
 
 dataframe = pd.read_csv(uploaded_file)
     
-st.write(dataframe)
+st.write(dataframe['text'])
 
 # name =  st.text_input('enter your name')
 
