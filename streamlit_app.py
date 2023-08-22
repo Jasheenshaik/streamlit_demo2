5edad1b5f25631b5975d176765a598cdd637e340
Jasheenshaik <77780331+Jasheenshaik@users.noreply.github.com>
2023-08-22 17:08:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.title("This is a demo website for the llm checking")
 
 
 def main():
-    # model_name = 'bert-base-uncased'
-    # tokenizer = AutoTokenizer.from_pretrained(model_name) 
+    model_name = 'bert-base-uncased'
+    tokenizer = AutoTokenizer.from_pretrained(model_name) 
     uploaded_file = st.file_uploader(label = 'upload the file')
     if uploaded_file is not None:
         try:
